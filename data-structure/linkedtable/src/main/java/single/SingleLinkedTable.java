0027e3c3996a59bad26ae7cf6abb10df8024fa91
yuzhou.jiang <yuzhou.jiang@tw.com>
2022-11-30 14:02:46 +0800

    add interface method

--- data-structure/linkedtable/src/main/java/single/SingleLinkedTable.java
+++ data-structure/linkedtable/src/main/java/single/SingleLinkedTable.java
@@ -8,4 +8,51 @@ public class SingleLinkedTable<T> {
 
     private Long size = 0L;
 
+    public void addFirst(T value) {
+
+    }
+
+    public void add(int index, T target) {
+
+    }
+
+    public void addLast(T value) {
+
+    }
+
+    public T getFirst() {
+
+    }
+
+    public T findFirst(T value) {
+
+    }
+
+    public T get(int index) {
+
+    }
+
+    public T getLast() {
+
+    }
+
+    public void removeFirst() {
+
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public void removeLast() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public Long size() {
+        return size;
+    }
 }
